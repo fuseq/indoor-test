3c3e681bfbc54638ead1a2cca00f230730ccd2ad
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-08-14 10:58:08 +0300

    .

--- script.js
+++ script.js
@@ -12,9 +12,8 @@ function staticLoadPlaces() {
         {
             name: 'Pokèmon',
             location: {
-                // decomment the following and add coordinates:
-                // lat: <your-latitude>,
-                // lng: <your-longitude>,
+                lat: 37.42555,
+                lng: 31.85118,
             },
         },
     ];
