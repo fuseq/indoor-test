fad8208506de19c75dc5955b60975c5d6e39eee1
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-09-17 09:11:38 +0300

    .

--- script.js
+++ script.js
@@ -80,28 +80,3 @@
         const div = document.querySelector('.instructions');
         div.innerText = model.info;
     };
-
-    function renderPlaces(places) {
-        let scene = document.querySelector('a-scene');
-
-        places.forEach((place) => {
-            let latitude = place.location.lat;
-            let longitude = place.location.lng;
-
-            let model = document.createElement('a-entity');
-            model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-            setModel(models[modelIndex], model);
-
-            model.setAttribute('animation-mixer', '');
-
-            document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-                var entity = document.querySelector('[gps-entity-place]');
-                modelIndex++;
-                var newIndex = modelIndex % models.length;
-                setModel(models[newIndex], entity);
-            });
-
-            scene.appendChild(model);
-        });
-    }
